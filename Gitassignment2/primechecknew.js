3087f1e449b2c26475356168e05c804079868b68
Rajat <rajat791753@gmail.com>
2023-02-21 21:46:20 +0530

    changed

--- Gitassignment2/primechecknew.js
+++ Gitassignment2/primechecknew.js
@@ -1,11 +1,11 @@
 let no =4
 let count =0
-for(let i=1;i<=no;i++){
+for(let i=2;i<=no;i++){
   if(no%i==0){
     count++
   }
 }
-if(count==2){
+if(count===1){
   console.log("It is Prime Number")
 }
 else{
